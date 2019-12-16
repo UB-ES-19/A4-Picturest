5aac3431abaade2f8275e94db1c103899c8757fe
Carolin-MM <31952519+Carolin-MM@users.noreply.github.com>
2019-12-16 20:10:28 +0100

    Update forms.py

--- Picturest/forms.py
+++ Picturest/forms.py
@@ -131,24 +131,6 @@ class InterestsSimpleShowForm(forms.ModelForm):
                   "paint", "travel", "garden", "photography"]
 
 
-class NotificationAcceptedForm(forms.ModelForm):
-    class Meta:
-        model = Notification
-        fields = ["user", "type", "friendship"]
-
-
-class NotificationRequestForm(forms.ModelForm):
-    class Meta:
-        model = Notification
-        fields = ["user", "type", "friendship"]
-
-
-class NotificationRePinForm(forms.ModelForm):
-    class Meta:
-        model = Notification
-        fields = ["user", "type", "friendship", "pin"]
-
-
 class RePinForm(forms.ModelForm):
     class Meta:
         model = RePin
